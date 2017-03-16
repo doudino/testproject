5bdbdab76efdf4556785ea19853b2b8d3dd3b1a6
Pierre v <toto@oto.fr>
2017-03-16 15:30:40 +0100

    introduction boucle

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python2
+
+def repeat(x,callback):
+    for _ in range(x):
+        callback()
+        
 def greetings():
     print "bonjour"
 
 if __name__ == "__main__":
-    greetings()
+    repeat(3,greetings)
